e126c03afe7b47c03fee51d2eddc2c4ffe1036f9
Willian van der Velde <mail@willian.io>
2014-12-25 17:28:50 +0100

    Product properties one liner

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -4,10 +4,6 @@ Spree::Product.class_eval do
   private
 
   def update_hstore_data_field
-    property_data = {}
-    product_properties.each do |product_property|
-      property_data[product_property.property.name] = product_property.value
-    end
-    self.data = property_data
+    self.data = Hash[product_properties.map { |pp| [pp.property.name, pp.value] }]
   end
 end
